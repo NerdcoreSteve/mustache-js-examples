a2db1b8624e4111b8adbb505ad6f7ccca18334ff
Steve Smith <steve@appdetex.com>
2015-05-07 11:26:13 -0600

    completed repeat example

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # mustache-js-examples
 examples for how to use mustache js
 
-Need node and express installed.
+https://github.com/janl/mustache.js/
+https://mustache.github.io/mustache.5.html
+
+Need node and express installed to run external template examples
 
 I'm pretty sure the npm install warnings are ok
--- public/repeat/scripts/main.js
+++ public/repeat/scripts/main.js
@@ -9,13 +9,18 @@ requirejs.config({
 require(["jquery", "mustache", "text!../templates/space_ships.tmpl"],
         function(jquery, mustache, madlibs_template) {
 
+    //Could just as easily come from an ajax call
     var data = {space_ships: [{name: 'Enterprise',
                                universe: 'Star Trek',
                                radness: '9'},
                               {name: 'X-Wing',
                                universe: 'Star Wars',
-                               radness: '8'}]};
+                               radness: '8'},
+                              {name: 'Trimaxium Drone Ship',
+                               universe: 'Flight Of The Navigator',
+                               radness: '6'}]};
 
-    $('body').html(mustache.render(madlibs_template, data));
+    $('body').html(mustache.render(madlibs_template, data)); //populated
+    //$('body').html(mustache.render(madlibs_template, {})); //default values shown
 
 });
